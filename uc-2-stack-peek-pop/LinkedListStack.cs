dcc1e767d99d105c807df3fd42df88fef6e96aaf
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-12 17:00:58 +0530

    added uc-2

---
+++ uc-2-stack-peek-pop/LinkedListStack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace day_14_linkedlist_stack_and_queue
+{
+    internal class LinkedListStack
+    {
+        public LinkedListStack()
+        {
+        }
+
+        internal void push(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Display()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
