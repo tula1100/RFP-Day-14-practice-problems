b4c93350b62dc0b8299c541712853e8e3d9f09e2
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-12 16:57:34 +0530

    added uc-1

---
+++ uc-1-LinkedList/push.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace day_14_linkedlist_stack_and_queue
+{
+    internal class Push
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PushING values to Stack");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.push(70);
+            linkedListStack.push(30);
+            linkedListStack.push(56);
+            linkedListStack.Display();
+
+        }
+    }
+}
